dc9de5f91e6b98665e9406f4bb4767bdd4a7b11a
apple <apple@MacBook.home>
2020-06-15 10:05:50 -0500

    Updates

---
+++ precache-manifest.4cd23fe31d73f10f65e60657b518885c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0cb57109adc3aae6741a8a8eec7d9e",
+    "url": "/seinfeld-quotes/index.html"
+  },
+  {
+    "revision": "dc66d4727eed9b585456",
+    "url": "/seinfeld-quotes/static/css/main.e0b8f381.chunk.css"
+  },
+  {
+    "revision": "39195b0a63d69890ae6d",
+    "url": "/seinfeld-quotes/static/js/2.036ba4d1.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/seinfeld-quotes/static/js/2.036ba4d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc66d4727eed9b585456",
+    "url": "/seinfeld-quotes/static/js/main.2016358c.chunk.js"
+  },
+  {
+    "revision": "05b93dfbe4c67cea73de",
+    "url": "/seinfeld-quotes/static/js/runtime-main.426ea006.js"
+  }
+]);
--- src/services/quotes-api.jsx
+++
@@ -1,5 +0,0 @@
-const BASE_URL = "https://seinfeld-quotes.herokuapp.com/quotes";
-
-export function getAll() {
-  return fetch(BASE_URL).then((res) => res.json());
-}
